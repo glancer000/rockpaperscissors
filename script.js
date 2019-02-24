d38277e8bc1b127a654d73249d1d6214f8862ef7
glancer000 <glancer000@gmail.com>
2019-02-24 15:11:24 -0300

    added win / lose logic

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
+const img = document.createElement("img");
+img.src = "letsplay.jpg"
+var src = document.getElementById("opponent");
+src.appendChild(img);
 
-function play(){
-    if(img == true){
-        img.src = option + ".png"
+const yourChoiceScissors = document.getElementById("scissors");
+const yourChoicePaper = document.getElementById("paper");
+const yourChoiceRock = document.getElementById("rock");
+
+
+function opponentChoice(){
+    let opponentChoice = Math.floor((Math.random() *3) + 1)
+
+    if(opponentChoice == 1){
+        option = "rock" 
+    }else if(opponentChoice == 2){
+        option = "paper" 
     }else{
-        let opponentChoice = Math.floor((Math.random() *3) + 1)
+    option = "scissors" }
 
-        if(opponentChoice == 1){
-            option = "rock" 
-        }else if(opponentChoice == 2){
-            option = "paper" 
-        }else{
-        option = "scissors" 
-        }
+    img.src = option + ".png"
+    return option
 
-        const img = document.createElement("img");
-        img.src = option + ".png"
-        var src = document.getElementById("opponent");
-        src.appendChild(img);
+}
+
+console.log(opponentChoice())
+
+yourChoicePaper.onclick = function(){
+    opponentChoice()
+
+    if(option == "paper"){
+        console.log("it's a tie")
     }
+    if(option == "rock"){
+        console.log("Paper covers rock. You win!")
+    }
+    if(option == "scissors"){
+        console.log("Scissors cut paper. You lose!")
+    }
+}
 
+yourChoiceRock.onclick = function(){
+    opponentChoice()
+    if(opponentChoice == yourChoiceRock){
+        console.log("it's a tie")
+    }
+}
+
+yourChoiceScissors.onclick = function(){
+    opponentChoice()
+    if(opponentChoice == yourChoiceScissors){
+        console.log("it's a tie")
+    }
 }
 
 
 
 
+// function play(){
+
+//     opponentChoice()
+
+//     if(img.src == "scissors.png"){
+//         alert("Tie")
+//     }else if (img.src == "rock.png"){
+//         alert("Rock crushes scissors. You lose")
+//     }else(img.src == "paper.png")
+//         alert("You win! Scissors cuts paper")
+    
+
+// }
 
 
